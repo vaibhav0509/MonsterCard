58033e6171b8e2f60cf140fd943a7f77357555e6
Vaibhav <vaibhav.mishra0509@gmail.com>
2021-01-19 17:10:57 +0530

    Updates

---
+++ precache-manifest.ae87df18de3132bea265f57698c1770b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90b492714f875d1102eac9cdba86247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "532ee7bc9f821e46f69e",
+    "url": "/static/css/main.6902ef38.chunk.css"
+  },
+  {
+    "revision": "f32f02700ffeaf5a2b14",
+    "url": "/static/js/2.bc3e73c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc3e73c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532ee7bc9f821e46f69e",
+    "url": "/static/js/main.b0032c75.chunk.js"
+  },
+  {
+    "revision": "e2cf8b8cff80fa455d09",
+    "url": "/static/js/runtime-main.0f71ade6.js"
+  }
+]);
